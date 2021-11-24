124252c6f9ed15f75aebf181202bc535d5803bcc
Felipe <felipepirovano95@gmail.com>
2021-11-24 13:02:30 -0300

    home style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import Head from 'next/head'
 
+import style from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={style.contentContainer}>
+        <section className={style.hero}>
+          <span>👏 Hey,Welcome</span>
+          <h1>News about that</h1>
+          <p>
+            Get acess to all the publications <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girld Colding" />
+      </main>
     </>
   )
 }
