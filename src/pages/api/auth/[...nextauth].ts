b769be587a70f1b4fa40c38775bd86e20550320b
Felipe <felipepirovano95@gmail.com>
2021-11-25 09:44:37 -0300

    pepino do fauna resolvido

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -8,5 +11,18 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET_API_KEY,
       scope: 'read:user'
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log(email)
+
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }))
+        return true
+      } catch (err) {
+        console.log(err)
+        return false
+      }
+    }
+  }
 })
